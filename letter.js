f94656f7800160ca3ddc35ea9d9e851bbc1c0dc6
unknown <aguirreh8@gmail.com>
2018-02-14 21:29:08 -0500

    added hangMan function. Added setWordBlank and wordProgres to letter.js. Added setWord to word.js.

--- letter.js
+++ letter.js
@@ -12,12 +12,23 @@ Letter.prototype.setWordBlank = function() {
 
 Letter.prototype.letterGuessed = function(letterUsed) {
 	this.letterBank.push(letterUsed);
+	console.log(`Letters used: ${this.letterBank}`);
 	for (let i = 0; i < this.word.length; i++) {
 		if (letterUsed === this.word.charAt(i)) {
 			this.wordBlank[i] = letterUsed;
 		}
 	}
+
 	console.log(this.wordBlank);
+	console.log("--------------------------");
+};
+
+Letter.prototype.wordProgress = function() {
+	let wordProgress = "";
+	for (let j = 0; j < this.wordBlank.length; j++) {
+		wordProgress = wordProgress + this.wordBlank[j];	
+	};
+	return wordProgress;	
 }
 
 module.exports = Letter;
--- main.js
+++ main.js
@@ -1,22 +1,40 @@
 const Word = require("./word.js");
 const Letter = require("./letter.js");
+const inquirer = require("inquirer");
+
+let tries = 9;
 
 const myWord = new Word();
-
 myWord.selectWordAtRandom();
 
 const letters = new Letter(myWord.word);
 letters.setWordBlank();
 
-console.log(myWord.word);
-console.log(letters.wordBlank);
-letters.letterGuessed("p");
 console.log(letters.wordBlank);
 
-// for (let i = 0; i < myWord.wordBlank.length; i++) {
-// 	wordBlankDispaly = wordBlankDispaly + myWord.wordBlank[i] + " ";
-// };
+const hangMan = () => {
+	if (tries > 0) {
+		inquirer.prompt([
+			{
+				name:"playerGuess",
+				message:"Type a letter",
+				type:"input"
+			}
+		]).then(function(response) {
+			letters.letterGuessed(response.playerGuess);
+			const wordProgress = letters.wordProgress();
+			if (myWord.word === wordProgress) {
+				console.log("You Win");
+				return;
+			} else {
+				tries--;
+				console.log(`Tries left: ${tries}.`);
+				hangMan();
+			}
+		})
+	} else {
+		console.log(`You loose! The word was ${myWord.word}`);
+	};
+};
 
-// const letters = new Letter(myWord.word, myWord.wordBlank);
-
-// letters.letterGuessed("y");
+hangMan();
